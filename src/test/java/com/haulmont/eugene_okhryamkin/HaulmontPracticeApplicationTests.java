6e31dc7608b7ef5558f205094fd4f7714ba5c17c
Eugene Okhryamkin <eugene.okhryamin@gmail.com>
2022-06-26 16:00:44 +0300

    init project

---
+++ src/test/java/com/haulmont/eugene_okhryamkin/HaulmontPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haulmont.eugene_okhryamkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulmontPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
