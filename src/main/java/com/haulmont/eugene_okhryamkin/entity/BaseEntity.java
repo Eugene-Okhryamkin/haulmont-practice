a9abee396ff8f1ecd0d5bf846b86667b789e211a
Eugene Okhryamkin <eugene.okhryamin@gmail.com>
2022-06-28 10:40:38 +0300

    base model

--- src/main/java/com/haulmont/eugene_okhryamkin/entity/BaseEntity.java
+++ src/main/java/com/haulmont/eugene_okhryamkin/entity/BaseEntity.java
@@ -1,18 +1,26 @@
 package com.haulmont.eugene_okhryamkin.entity;
 
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.util.UUID;
 
+@JmixEntity
 @MappedSuperclass
 public class BaseEntity {
-
     @JmixGeneratedValue
-    @Column(name="ID", nullable = false)
+    @Column(name = "ID", nullable = false)
     @Id
     private UUID id;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
